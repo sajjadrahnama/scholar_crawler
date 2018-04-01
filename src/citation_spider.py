e99038bfa9b9e05d9749c87f81900f3357fd30fe
Sajjad <sajjad.rahnama7@gmail.com>
2018-04-01 13:26:27 +0430

    Exception Handling

--- src/citation_spider.py
+++ src/citation_spider.py
@@ -30,20 +30,24 @@ class CitationSpider(scrapy.Spider):
         log(response)
         scholar_url = 'https://scholar.google.com'
         for obj in response.css('.gs_r.gs_or.gs_scl .gs_ri'):
-            authors, year = extract_authors(obj.css('.gs_a').xpath('string(.)').extract_first())
-            data = {
-                'title': obj.css('.gs_rt a').xpath('string(.)').extract_first(),
-                'link': obj.css('.gs_rt a::attr(href)').extract_first(),
-                'authors': authors,
-                'year': year,
-                'abstract': obj.css('.gs_rs').xpath('string(.)').extract_first(),
-                'citations': int(re.search('(\d+)', obj.css('.gs_fl').xpath('string(a[3])').extract_first()).group(1)),
-                'citations_link': scholar_url + obj.css('.gs_fl').xpath('a[3]/@href').extract_first() + '&as_vis=1',
-                'citations_flag': True,
-            }
-            model = ArticleModel(data)
-            model.save()
-            self.cite(model.id, response.meta['article']['_id'])
+            try:
+                authors, year = extract_authors(obj.css('.gs_a').xpath('string(.)').extract_first())
+                data = {
+                    'title': obj.css('.gs_rt a').xpath('string(.)').extract_first(),
+                    'link': obj.css('.gs_rt a::attr(href)').extract_first(),
+                    'authors': authors,
+                    'year': year,
+                    'abstract': obj.css('.gs_rs').xpath('string(.)').extract_first(),
+                    'citations': int(
+                        re.search('(\d+)', obj.css('.gs_fl').xpath('string(a[3])').extract_first()).group(1)),
+                    'citations_link': scholar_url + obj.css('.gs_fl').xpath('a[3]/@href').extract_first() + '&as_vis=1',
+                    'citations_flag': True,
+                }
+                model = ArticleModel(data)
+                model.save()
+                self.cite(model.id, response.meta['article']['_id'])
+            except Exception:
+                pass
         start = int(re.findall('start=([\d]+)', response.url)[0]) + 10
         next_page = re.sub('start=[0-9]*$', 'start=' + str(start), response.url)
 
@@ -66,12 +70,14 @@ def extract_authors(raw):
     return raw, year
 
 
-def log(response):
+def log(response, error=-1):
     res = re.findall('start=([\d]+)', response.url)
     start = res[0]
     res = str(response.status) + '\t'
     res += time.strftime("%m-%d %H:%M:%S", time.localtime()) + '\t'
     res += start + '\t'
     res += str(response.meta['article']['_id']) + '\n'
+    if error != -1:
+        res = '\nERROR\t' + error + '\n' + res
     with open("logs/citation.txt", "a") as file:
         file.write(res)
--- src/main_spider.py
+++ src/main_spider.py
@@ -28,22 +28,28 @@ class MainSpider(scrapy.Spider):
         i = 1
         scholar_url = 'https://scholar.google.com'
         for obj in response.css('.gs_r.gs_or.gs_scl .gs_ri'):
-            authors, year = extract_authors(obj.css('.gs_a').xpath('string(.)').extract_first())
-            data = {
-                'title': obj.css('.gs_rt a').xpath('string(.)').extract_first(),
-                'link': obj.css('.gs_rt a::attr(href)').extract_first(),
-                'topic': self.topic,
-                'index': self.start + i,
-                'authors': authors,
-                'year': year,
-                'abstract': obj.css('.gs_rs').xpath('string(.)').extract_first(),
-                'citations': int(re.search('(\d+)', obj.css('.gs_fl').xpath('string(a[3])').extract_first()).group(1)),
-                'citations_link': scholar_url + obj.css('.gs_fl').xpath('a[3]/@href').extract_first() + '&as_vis=1',
-                'citations_flag': False,
-            }
-            model = ArticleModel(data)
-            model.save()
-            i += 1
+            try:
+                authors, year = extract_authors(obj.css('.gs_a').xpath('string(.)').extract_first())
+                citations = re.search('(\d+)', obj.css('.gs_fl').xpath('string(a[3])').extract_first())
+                if citations:
+                    citations = int(citations.group(1))
+                data = {
+                    'title': obj.css('.gs_rt a').xpath('string(.)').extract_first(),
+                    'link': obj.css('.gs_rt a::attr(href)').extract_first(),
+                    'topic': self.topic,
+                    'index': self.start + i,
+                    'authors': authors,
+                    'year': year,
+                    'abstract': obj.css('.gs_rs').xpath('string(.)').extract_first(),
+                    'citations': citations,
+                    'citations_link': scholar_url + obj.css('.gs_fl').xpath('a[3]/@href').extract_first() + '&as_vis=1',
+                    'citations_flag': False,
+                }
+                model = ArticleModel(data)
+                model.save()
+                i += 1
+            except Exception:
+                log(response, i)
         self.start += 10
         next_page = re.sub('start=[0-9]*$', 'start=' + str(self.start), response.url)
 
@@ -67,7 +73,7 @@ def insert_topic(topic):
         collection.insert_one({'topic': topic})
 
 
-def log(response):
+def log(response, error=-1):
     res = re.findall('q=([\w+]*).*start=([\d]+)', response.url)
     topic = res[0][0].replace('+', ' ')
     start = res[0][1]
@@ -75,5 +81,7 @@ def log(response):
     res += time.strftime("%m-%d %H:%M:%S", time.localtime()) + '\t'
     res += start + '\t'
     res += topic + '\n'
+    if error != -1:
+        res = '\nERROR\t' + error + '\n' + res
     with open("logs/main.txt", "a") as file:
         file.write(res)
