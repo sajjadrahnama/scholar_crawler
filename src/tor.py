9fbbe23d6cd62df0df1094aff719f3ed3930ce81
Sajjad <sajjad.rahnama7@gmail.com>
2018-04-01 18:49:59 +0430

    Tor Exception

--- src/tor.py
+++ src/tor.py
@@ -5,6 +5,9 @@ from conf import torControlPassword
 
 def change_ip():
     """Change IP using TOR"""
-    with Controller.from_port(port=9051) as controller:
-        controller.authenticate(password=torControlPassword)
-        controller.signal(Signal.NEWNYM)
+    try:
+        with Controller.from_port(port=9051) as controller:
+            controller.authenticate(password=torControlPassword)
+            controller.signal(Signal.NEWNYM)
+    except Exception:
+        pass
